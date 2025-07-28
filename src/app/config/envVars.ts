31c73e8a465c1292e5e5ee747bc9f4c6768434b5
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-07-28 22:35:56 +0600

    implement google login strategy via passportjs

--- src/app/config/envVars.ts
+++ src/app/config/envVars.ts
@@ -13,6 +13,11 @@ interface EnvVariables {
   JWT_REFRESH_EXPIRES: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
+  FRONTEND_URL: string;
+  EXPRESS_SESSION: string;
 }
 
 const requiredVariables = [
@@ -26,6 +31,10 @@ const requiredVariables = [
   "JWT_REFRESH_EXPIRES",
   "SUPER_ADMIN_EMAIL",
   "SUPER_ADMIN_PASSWORD",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_CLIENT_SECRET",
+  "GOOGLE_CALLBACK_URL",
+  "FRONTEND_URL",
 ];
 requiredVariables.forEach((key) => {
   if (!process.env[key]) {
@@ -44,4 +53,9 @@ export const envVars: EnvVariables = {
   JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
   SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
   SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+  FRONTEND_URL: process.env.FRONTEND_URL as string,
+  EXPRESS_SESSION: process.env.EXPRESS_SESSION as string,
 };
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import passport from "passport";
 import { AuthController } from "./auth.controller";
 
 const router = Router();
@@ -6,5 +7,22 @@ const router = Router();
 router.post("/login", AuthController.credentialLogin);
 router.post("/logout", AuthController.logout);
 router.post("/refresh-token", AuthController.getNewAccessToken);
+router.get(
+  "/google",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const redirect = req.query.redirect ? req.query.redirect : "/";
+    await passport.authenticate("google", {
+      scope: ["profile", "email"],
+      prompt: "consent",
+      state: redirect as string,
+    })(req, res, next);
+  }
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  AuthController.googleCallbackController
+);
 
 export const AuthRoutes = router;
